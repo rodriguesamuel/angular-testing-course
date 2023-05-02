30fe880dd44e3369f6d17985fac8284673c1aabd
Samuel <samuel.b.rodrigues@accenture.com>
2023-05-02 10:15:36 -0300

    data modification test

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -2,6 +2,7 @@ import {CoursesService} from "./courses.service";
 import {TestBed} from "@angular/core/testing";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {COURSES} from '../../../../server/db-data';
+import {Course} from "../model/course";
 
 describe('CoursesService', () => {
 
@@ -63,6 +64,24 @@ describe('CoursesService', () => {
     expect(req.request.method).toEqual("GET");
 
     req.flush(COURSES[12]);
+
+  });
+
+  it('should save the course', () => {
+
+    const changes: Partial<Course> = {titles: {description:'testing course'}};
+
+    coursesService.saveCourse(12, changes).subscribe(course=>{
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+
+    expect(req.request.method).toEqual('PUT');
+
+    expect(req.request.body.titles.description).toEqual(changes.titles.description);
+
+    req.flush({...COURSES[12], ...changes});
 
   });
 
