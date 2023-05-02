e9d0213226e96f032e1d60e4c5fe91d3965c26c1
Samuel <samuel.b.rodrigues@accenture.com>
2023-05-02 10:09:40 -0300

    first http service test, preventing unintending http resquests

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -3,7 +3,8 @@ import {TestBed} from "@angular/core/testing";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {COURSES} from '../../../../server/db-data';
 
-fdescribe('CoursesService', () => {
+describe('CoursesService', () => {
+
   let coursesService: CoursesService,
     httpTestingController: HttpTestingController;
 
@@ -16,7 +17,7 @@ fdescribe('CoursesService', () => {
       providers: [
         CoursesService
       ]
-    })
+    });
 
     coursesService = TestBed.get(CoursesService);
     httpTestingController = TestBed.get(HttpTestingController);
@@ -25,31 +26,48 @@ fdescribe('CoursesService', () => {
 
   it('should retrieve all courses', () => {
 
-
     coursesService.findAllCourses()
       .subscribe(courses => {
 
         expect(courses).toBeTruthy('No courses returned');
 
-        expect(courses.length).toBe(12, 'incorrect number of courses');
+        expect(courses.length).toBe(12,"incorrect number of courses");
 
         const course = courses.find(course => course.id == 12);
 
-        expect(course.titles.description).toBe('Angular Testing Course');
+        expect(course.titles.description).toBe(
+          "Angular Testing Course");
 
       });
 
     const req = httpTestingController.expectOne('/api/courses');
 
-    expect(req.request.method).toEqual('GET');
+    expect(req.request.method).toEqual("GET");
 
-    req.flush({payLoad: Object.values(COURSES)});
+    req.flush({payload: Object.values(COURSES)});
+
   });
 
-  afterEach(() => {
+  it('should find a course by id', () => {
 
+    coursesService.findCourseById(12)
+      .subscribe(course => {
+
+        expect(course).toBeTruthy();
+        expect(course.id).toBe(12);
+
+      });
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+
+    expect(req.request.method).toEqual("GET");
+
+    req.flush(COURSES[12]);
+
+  });
+
+  afterEach(()=>{
     httpTestingController.verify();
-  });
-
+  })
 
 });
