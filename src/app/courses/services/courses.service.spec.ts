2607fc6c2465a9d4bafc04fc70862077009c1ada
Samuel <samuel.b.rodrigues@accenture.com>
2023-05-02 10:24:01 -0300

    error handling test

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -3,6 +3,7 @@ import {TestBed} from "@angular/core/testing";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {COURSES} from '../../../../server/db-data';
 import {Course} from "../model/course";
+import {HttpErrorResponse} from "@angular/common/http";
 
 describe('CoursesService', () => {
 
@@ -32,7 +33,7 @@ describe('CoursesService', () => {
 
         expect(courses).toBeTruthy('No courses returned');
 
-        expect(courses.length).toBe(12,"incorrect number of courses");
+        expect(courses.length).toBe(12, "incorrect number of courses");
 
         const course = courses.find(course => course.id == 12);
 
@@ -69,9 +70,9 @@ describe('CoursesService', () => {
 
   it('should save the course', () => {
 
-    const changes: Partial<Course> = {titles: {description:'testing course'}};
+    const changes: Partial<Course> = {titles: {description: 'testing course'}};
 
-    coursesService.saveCourse(12, changes).subscribe(course=>{
+    coursesService.saveCourse(12, changes).subscribe(course => {
       expect(course.id).toBe(12);
     });
 
@@ -85,7 +86,28 @@ describe('CoursesService', () => {
 
   });
 
-  afterEach(()=>{
+  it('should give an error if save course fails', () => {
+
+    const changes: Partial<Course> = {titles: {description: 'testing course'}};
+
+    coursesService.saveCourse(12, changes).subscribe(
+      () => {
+        fail('the save course operationshold have fail')
+      },
+      (error: HttpErrorResponse) => {
+          expect(error.status).toBe(500);
+      }
+    );
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+
+    expect(req.request.method).toEqual('PUT');
+
+    req.flush('Save course failed', {status:500, statusText: 'Internal Server Error'});
+
+  });
+
+  afterEach(() => {
     httpTestingController.verify();
   })
 
