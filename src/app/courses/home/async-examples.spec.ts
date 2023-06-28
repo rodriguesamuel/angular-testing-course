a75600e1639c694cfe92353dc2b2635400d16bdc
Samuel <samuel.bruno.aguiar@gmail.com>
2023-06-27 22:56:47 -0300

    fakeAsync test utility

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,3 +1,5 @@
+import {fakeAsync, flush, tick} from "@angular/core/testing";
+
 fdescribe("Async Testing example", () => {
   it("async test example with jasmine done()", (done: DoneFn) => {
 
@@ -14,4 +16,25 @@ fdescribe("Async Testing example", () => {
     }, 1000);
 
   });
+
+  it("asynchronous test example - setTimeOut()", fakeAsync(() => {
+
+    let test = false;
+
+    setTimeout(()=>{});
+
+    setTimeout(() => {
+      console.log('running assertions timeOut()');
+
+      test = true;
+
+    }, 1000);
+
+    //tick(1000);
+
+    flush();
+
+    expect(test).toBeTruthy();
+
+  }));
 });
